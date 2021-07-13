37df39fd8dad08512682a40c86471303cc9c1b9a
AlexC01 <alexcuadraxxx@gmail.com>
2021-07-13 02:00:51 -0600

    dark mode

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,52 @@ const todos_list = document.querySelector('.listTodos');
 const footercard = document.getElementById('footer');
 const items_left = document.querySelector('.items_left');
 const labels_filter = document.querySelectorAll('.labels_filter');
+const light = document.getElementById('light');
+const dark = document.getElementById('dark');
+
+const change_theme = () =>{
+    if(localStorage.getItem('theme') == 'dark'){
+        document.documentElement.classList.add('dark');
+        dark.classList.remove('block');
+        dark.classList.add('hidden');
+        light.classList.add('block');
+        light.classList.remove('hidden');
+        document.body.classList.remove('light_bg');
+        document.body.classList.add('dark_bg');
+    }else{
+        document.documentElement.classList.remove('dark');
+        document.body.classList.add('light_bg');
+        document.body.classList.remove('dark_bg');
+    }
+}
+
+const theme = () => {
+    if(localStorage.getItem('theme') == 'dark'){
+        localStorage.setItem('theme', 'light');
+        light.classList.remove('block');
+        light.classList.add('hidden');
+        dark.classList.add('block');
+        dark.classList.remove('hidden');
+        change_theme();
+    }else{
+        localStorage.setItem('theme', 'dark');
+        dark.classList.remove('block');
+        dark.classList.add('hidden');
+        light.classList.add('block');
+        light.classList.remove('hidden');
+        change_theme();
+    }
+};
+
+window.onload = function(){
+    if(localStorage.getItem('theme')){
+        change_theme();
+        todosView();
+    }else{
+        localStorage.setItem('theme', 'light');
+        todosView();
+    }
+}
 
 const filterTodos = filtro => {
     let hijos = todos_list.children;
@@ -96,10 +142,10 @@ const todosView = () => {
                 cont++;
                 todos_list.innerHTML += 
                 `
-                <div class="todos grid grid-cols-4 p-5 border-b-2">
+                <div class="todos grid grid-cols-4 p-5 border-b-2 dark:border-gray-500">
                     <div class="flex justify-items-start col-span-3">
                         <button onclick="changeStatus(${i})" class="butons"></button>
-                        <p class="ml-6 text-lg w-full">${todos[i].description}</p>
+                        <p class="ml-6 text-lg w-full dark:text-verylight_grayishblue">${todos[i].description}</p>
                     </div>
                     <div class="equis hidden">
                         <button onclick="deleteTodo(${i})"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg></button>
@@ -109,10 +155,10 @@ const todosView = () => {
             }else{
                 todos_list.innerHTML += 
                 `
-                <div class="todos grid grid-cols-4 p-5 border-b-2 completed">
+                <div class="todos grid grid-cols-4 p-5 border-b-2 completed dark:border-gray-500">
                     <div class="flex justify-items-start col-span-3">
                         <button class="butons_completed" onclick="changeStatus(${i})"><svg xmlns="http://www.w3.org/2000/svg" width="11" height="9" style="margin-left: 9px;"><path fill="none" stroke="#FFF" stroke-width="2" d="M1 4.304L3.696 7l6-6"/></svg></button>
-                        <p class="ml-6 text-lg w-full line-through text-light_grayishblue">${todos[i].description}</p>
+                        <p class="ml-6 text-lg w-full line-through text-light_grayishblue dark:text-dark_grayish_blue">${todos[i].description}</p>
                     </div>
                     <div class="equis hidden">
                         <button onclick="deleteTodo(${i})"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg></button>
