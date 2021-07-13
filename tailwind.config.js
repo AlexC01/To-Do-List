5e31214c24c8490c8a6f9aa7ddf2ec1f92812f42
AlexC01 <alexcuadraxxx@gmail.com>
2021-07-12 21:34:47 -0600

    html y estilos light

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ module.exports = {
     fontFamily:{
       josefin: ['Josefin Sans', 'sans-serif'],
     },
-    extend: {},
+    extend: {
+      colors:{
+        bright_blue: '#3a7bfd',
+        background_1: '#57ddff',
+        background_2: '#c058f3',
+        verylight_gray: '#fafafa',
+        verylight_grayishblue: '#e4e5f1',
+        light_grayishblue: '#d2d3db',
+        dark_grayish_blue: '#9394a5',
+        verydark_blue_grayish: '#484b6a',
+        darkdark_blue: '#161722',
+        darkdark_desaturated: '#25273c',
+        darkdarklightgrayishblue: '#cacde8',
+        darkhovergrayishblue: '#e4e5f1',
+        darkdarkgrayishblue: '#777a92',
+        veryveryvery: '#4d5066',
+        veryvery_2: '#393a4c',
+      },
+    },
   },
   variants: {
     extend: {},
